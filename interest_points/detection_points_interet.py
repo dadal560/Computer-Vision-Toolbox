58dc963396a3a83ff8c270c197aab147d7ecefb8
dadal <gwen.henry56@gmail.com>
2026-03-22 02:07:07 +0100

    Amélioration de la détection des points d'intérêt en optimisant la création de l'ORB et en corrigeant les noms de variables pour une meilleure clarté.

--- interest_points/detection_points_interet.py
+++ interest_points/detection_points_interet.py
@@ -5,14 +5,14 @@ def main():
     cam = 0  
     cap = cv.VideoCapture(cam)
     cap.set(cv.CAP_PROP_FPS, 25)
+    orb = cv.ORB_create(nfeatures=500, scoreType=cv.ORB_FAST_SCORE)
     while True:
         ret, src = cap.read()
         gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
         # masque de taille 21*21
         gray = cv.GaussianBlur(gray, (21, 21), 0)
-        orb = cv.ORB_create()
-        keyPoint, des = orb.detectAndCompute(gray, None)
-        src = cv.drawKeypoints(src, keyPoint, None, color=(0, 255, 0), flags=0)
+        keypoints, descripteurs = orb.detectAndCompute(gray, None)
+        src = cv.drawKeypoints(src, keypoints, None, color=(0, 255, 0), flags=cv.DRAW_MATCHES_FLAGS_DEFAULT)
         cv.imshow("detect point interet", src)
         key = cv.waitKey(1)
         if key == 27:
@@ -24,6 +24,3 @@ def main():
  
 if __name__ == "__main__":
     main()
-
-
-    
--- interest_points/detection_points_interet_v2.py
+++ interest_points/detection_points_interet_v2.py
@@ -1,31 +1,30 @@
 import cv2 as cv
 
-FRAME_100 = "interest_points/points_interet.jpg"
+
+FRAME_100 = "Tp7/detected_frame.jpg"
 
 def main():
     cam = 0  
     cap = cv.VideoCapture(cam)
     cap.set(cv.CAP_PROP_FPS, 25)
     count_frame = 0
-    orb = cv.ORB_create(10000)
+    orb = cv.ORB_create()
+    frame100 = cv.imread(FRAME_100)
     while True:
         ret, src = cap.read()
         count_frame += 1
-        if count_frame == 50:
-            cv.imwrite(FRAME_100, src)
-            print ("Frame 50 saved as points_interet.jpg")
         gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
         gray = cv.GaussianBlur(gray, (21, 21), 0)
-        keyPoint, des = orb.detectAndCompute(gray, None)
-        img_src = cv.drawKeypoints(src, keyPoint, None, color=(0, 255, 0), flags=0)
+        keypoints, descripteurs = orb.detectAndCompute(gray, None)
+        img_src = cv.drawKeypoints(src, keypoints, None, color=(0, 255, 0), flags=0)
+        
         if count_frame >= 101 and count_frame <= 200:
             # frame100
-            frame100 = cv.imread(FRAME_100)
             gray100 = cv.cvtColor(frame100, cv.COLOR_BGR2GRAY)
             gray100 = cv.GaussianBlur(gray100, (21, 21), 0)
-            keyPoint100, des = orb.detectAndCompute(gray100, None)
+            keypoints100, descripteurs100 = orb.detectAndCompute(gray100, None)
 
-            img_out = cv.drawKeypoints(img_src, keyPoint100, None, color=(255, 0, 0), flags=0)
+            img_out = cv.drawKeypoints(img_src, keypoints100, None, color=(255, 0, 0), flags=0)
             cv.imshow("detect point interet", img_out)
         elif count_frame > 200:
             cv.destroyWindow("detect point interet")
@@ -40,5 +39,3 @@ def main():
  
 if __name__ == "__main__":
     main()
-
-    
